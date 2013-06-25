8dfb1e534ddec47e38c3ea3d87d174728f40fc09
Valeriy Vasin <valeriy.vasin@sup.com>
2013-06-25 04:16:51 +0400

    Implement merging and minification of source files.

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        filename: 'jasmine-package',
+
+        files: {
+            css: 'components/jasmine/lib/jasmine-core/jasmine.css',
+            core: 'components/jasmine/lib/jasmine-core/jasmine.js',
+            reporter: 'components/jasmine/lib/jasmine-core/jasmine-html.js',
+
+            output: 'jasmine-package.js'
+        },
+
+        uglify: {
+            min: {
+                options: {
+                    mangle: false
+                },
+                files: {
+                    'jasmine-package.min.js': 'jasmine-package.js'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('build', function () {
+        var tmpl = grunt.file.read('jasmine-package.tmpl'),
+            data = {};
+
+        data.core = grunt.file.read( grunt.config('files.core') );
+        data.reporter = grunt.file.read( grunt.config('files.reporter') );
+        data.css = grunt.file.read( grunt.config('files.css') );
+
+        data.css = data.css
+            .replace(/\n+/g, '')
+            .replace(/('|")/g, '\\$1');
+
+        grunt.file.write(
+            grunt.config('files.output'),
+            grunt.template.process(tmpl, { data: data })
+        );
+
+        grunt.task.run('uglify');
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+};
